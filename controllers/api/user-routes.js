d046ccc75db1553e8ee48125aee11fa7591f40b0
Ge Su <gegege.su@gmail.com>
2023-06-22 22:37:45 +1000

    adjusted controllers, server file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,62 @@
+// const router = require('express').Router();
+// const { User, Inventory } = require('../../models')
+
+// router.get('/', async (req, res) => {
+//     try{
+//         const userData = await User.findAll({
+
+//         })
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
+// module.exports = router;
+
 const router = require('express').Router();
-const { User, Inventory } = require('../../models')
+const { User } = require('../../models');
 
-router.get('/', async (req, res) => {
-    try{
-        const userData = await User.findAll({
+router.post('/login',  async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-        })
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
-})
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
