e26a9bb17691fc7a2567849084224c4f0edfce8d
BenC <bcuttance1998@gmail.com>
2023-06-27 18:32:56 +0930

    added javascript and route for adding and taking stock

--- controllers/api/inventory-routes.js
+++ controllers/api/inventory-routes.js
@@ -1,53 +1,62 @@
-// const router = require('express').Router();
-// const { User, Inventory } = require('../../models')
+const router = require('express').Router();
+const { User, Inventory } = require('../../models')
 
 
 // // Get all items in inventory
-// router.get('/', async (req, res) => {
-//     try{
-//         const inventoryData = await Inventory.findAll({
+router.get('/', async (req, res) => {
+    try {
+        const inventoryData = await Inventory.findAll({
 
-//         })
-//         res.status(200).json(inventoryData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
+        })
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
-// router.get('/:id', async (req, res) => {
-//     try{
-//         const inventoryData = await Inventory.findByPk(req.params.id, {
-        
-//         })
-//         console.log(inventoryData);
-//         if (!inventoryData){
-//             res.status(404).json({ message: 'No item with this id'})
-//             return;
-//         }
-//         res.status(200).json(inventoryData)
-//     } catch (err) {
-//         res.status(500).send(err)
-//     }
-// })
+router.get('/:id', async (req, res) => {
+    try {
+        const inventoryData = await Inventory.findByPk(req.params.id, {
 
-// // Update item in inventory
-// router.put('/:id', async (req, res) => {
-//     try {
-//         const inventoryData = await Inventory.update(req.body, {
-//             where: {
-//                 id: req.params.id
-//             }
-//         })
-//         if (!inventoryData){
-//             res.status(404).json({ message: 'No item with this Id'})
-//             return;
-//         }
-//         res.status(200).json(inventoryData)
-//     } catch (err) {
-//         console.log(err)
-//         res.status(500).json(err);
-//     }
-    
-// })
+        })
+        console.log(inventoryData);
+        if (!inventoryData) {
+            res.status(404).json({ message: 'No item with this id' })
+            return;
+        }
+        res.status(200).json(inventoryData)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 
-// module.exports = router;
+// Update item in inventory
+router.post('/:id', async (req, res) => {
+    try {
+        console.log(req.body)
+        let inventoryData = await Inventory.findByPk(req.params.id)
+        if (req.body.increment) {
+            inventoryData = await inventoryData.increment("quantity", { by: req.body.increment })
+        }
+        else if (req.body.decrement) {
+            inventoryData = await inventoryData.decrement("quantity", { by: req.body.decrement })
+        }
+        else if (req.body.quantity !== null && !isNaN (req.body.quantity) ) {
+            inventoryData.quantity = req.body.quantity
+            inventoryData = await inventoryData.save()
+        }
+
+        if (!inventoryData) {
+            res.status(404).json({ message: 'No item with this Id' })
+            return;
+        }
+        res.status(200).json(inventoryData)
+        // let log = await log.create
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+
+})
+
+module.exports = router;
